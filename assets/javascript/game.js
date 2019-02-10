c39dcc44f93f9d33635f5ca524348fbf686f293f
Weslley Braga <wesbragagt@gmail.com>
2019-02-10 12:56:35 -0600

    added more functionality to my psych game and a theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,20 +7,27 @@ console.log("array contains " + test + " letters");
 
 // make variables for wins, lossess, guesses left and guessess so far.
 var win = 0;
-var lossess;
-var guessesLeft;
+var lossess = 0;
+
+var guessesLeft = 12;
+
 var letterSoFar = [];
+
 var keyPressed = document.getElementById("letter-guessed");
 var computerGuess = generateRandomLetter();
+console.log("computer picks " + computerGuess);
+
 // 2. computer chooses from those letters
 function generateRandomLetter() {
     var computerNumber = Math.floor(Math.random() * letters.length);
     return letters[computerNumber];
+
 }
-console.log("letter generated was " + computerGuess);
+
 
 
 // 3. make a keypress function to sense the keys being pressed
+
 document.onkeyup = function (event) {
 
     letterSoFar.unshift(event.key);
@@ -28,16 +35,33 @@ document.onkeyup = function (event) {
     newLetter.textContent = letterSoFar[0];
     keyPressed.appendChild(newLetter);
 
+    // 4. if one of the letters pressed on keyboard is equals to letter computer picked add +1 to wins and pick another letter
+
     if (letterSoFar[0] == computerGuess) {
-        alert("win");
+        alert("OOOOH NOO, You got me");
+        win = win + 1;
+        document.getElementById("wins").innerHTML = win;
+        location.reload();
+
+
 
     }
     else {
-        alert("you lose");
+        alert("haha wrong letter, psyyyych!");
+        lossess = lossess + 1;
+        guessesLeft = guessesLeft - 1;
+        document.getElementById("guesses-left").innerHTML = guessesLeft;
+
+        if (guessesLeft === 0) {
+            alert("GAMEOVER");
+            document.write("<h1>Game Over</h1>");
+        }
+        else { return; }
     }
 }
 
-// 4. if one of the letters pressed on keyboard is equals to letter computer picked add +1 to wins and pick another letter
+
+
 
 
 
